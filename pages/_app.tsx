39b38a5a22c0a3be481528408a8162d7e2ab4500
Greg Ogun <gregogun97@gmail.com>
2021-11-17 09:44:16 +0000

    feat: add theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { darkTheme } from '../stitches.config';
+import { ThemeProvider } from 'next-themes';
+import { AppProps } from 'next/app';
+import React, { FC } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      disableTransitionOnChange
+      attribute="class"
+      defaultTheme="system"
+      value={{ light: 'light-theme', dark: darkTheme.toString() }}
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
