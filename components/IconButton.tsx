f8954d58736cc8dab2bce44ee5234face77cd51b
Greg Ogun <gregogun97@gmail.com>
2021-11-17 20:34:42 +0000

    feat: add icon button

---
+++ components/IconButton.tsx
@@ -0,0 +1,43 @@
+import { resets } from '@/styles/resets/button';
+import { mauve } from '@radix-ui/colors';
+import { css, styled } from 'stitches.config';
+
+const iconButton = css({
+  //   ...resets,
+  cursor: 'pointer',
+  border: 'none',
+  padding: 0,
+  margin: 0,
+  outline: 'none',
+  textDecoration: 'none',
+  alignItems: 'center',
+  appearance: 'none',
+  boxSizing: 'border-box',
+  display: 'inline-flex',
+  userSelect: 'none',
+  WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+  borderRadius: '$sm',
+
+  boxShadow: `inset 0 0 0 1px var(--colors-elementBorder), 0 0 0 1px var(--colors-elementBorder)`,
+  backgroundColor: '$elementBg',
+
+  variants: {
+    size: {
+      md: {
+        p: '$3',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
+});
+
+const IconButtonBase = styled('button', {
+  ...iconButton,
+});
+
+export const IconButton = ({ children, ...props }: any) => {
+  return <IconButtonBase {...props}>{children}</IconButtonBase>;
+};
