45f949536a2c3f2f9fd0e8edca29193565f31fe5
Greg Ogun <gregogun97@gmail.com>
2021-11-17 20:29:50 +0000

    chore: add theme tokens
    add colors, size, spacing, radii, and border styles to theme config

--- stitches.config.ts
+++ stitches.config.ts
@@ -1,10 +1,53 @@
-import {
-  createStitches,
-  PropertyValue,
-  ScaleValue,
-  CreateStitches,
-} from '@stitches/react';
+import { createStitches, PropertyValue, ScaleValue } from '@stitches/react';
 import type * as Stitches from '@stitches/react';
+import { mauve, mauveDark, violet, violetDark } from '@radix-ui/colors';
+
+const spaceTokens = {
+  px: '1px',
+  // 0.5: "0.125rem",
+  1: '0.25rem',
+  // 1.5: "0.375rem",
+  2: '0.5rem',
+  // 2.5: "0.625rem",
+  3: '0.75rem',
+  // 3.5: "0.875rem",
+  4: '1rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2rem',
+  9: '2.25rem',
+  10: '2.5rem',
+  12: '3rem',
+  14: '3.5rem',
+  16: '4rem',
+  20: '5rem',
+  24: '6rem',
+  28: '7rem',
+  32: '8rem',
+  36: '9rem',
+  40: '10rem',
+  44: '11rem',
+  48: '12rem',
+  52: '13rem',
+  56: '14rem',
+  60: '15rem',
+  64: '16rem',
+  72: '18rem',
+  80: '20rem',
+  96: '24rem',
+};
+
+const darkModeConfig = {
+  colors: {
+    AppBg: mauveDark.mauve1,
+    loContrast: mauveDark.mauve11,
+    hiContrast: mauveDark.mauve12,
+    muted: mauveDark.mauve9,
+    elementBg: mauveDark.mauve3,
+    elementBorder: mauveDark.mauve7,
+  },
+};
 
 export const {
   styled,
@@ -16,9 +59,20 @@ export const {
   keyframes,
 } = createStitches({
   theme: {
-    colors: {},
-    space: {},
-    sizes: {},
+    colors: {
+      AppBg: mauve.mauve1,
+      loContrast: mauve.mauve11,
+      hiContrast: mauve.mauve12,
+      muted: mauve.mauve9,
+      elementBg: mauve.mauve3,
+      elementBorder: mauve.mauve7,
+    },
+    space: {
+      ...spaceTokens,
+    },
+    sizes: {
+      ...spaceTokens,
+    },
     fontSizes: {
       xs: '0.75rem',
       sm: '0.875rem',
@@ -56,8 +110,17 @@ export const {
     },
     letterSpacings: {},
     borderWidths: {},
-    borderStyles: {},
-    radii: {},
+    borderStyles: {
+      subtle:
+        'inset 0 0 0 1px var(--colors-elementBorder), 0 0 0 1px var(--colors-elementBorder)',
+    },
+    radii: {
+      xs: '2px',
+      sm: '4px',
+      md: '8px',
+      lg: '24px',
+      full: '9999px',
+    },
     shadows: {},
     transitions: {},
     zIndices: {},
@@ -137,10 +200,6 @@ export const {
     }),
   },
 });
-
-const darkModeConfig = {
-  colors: {},
-};
 
 export const darkTheme = createTheme('darkTheme', darkModeConfig);
 export type CSS = Stitches.CSS<typeof config>;
