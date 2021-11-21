6d68ef59d9fbb542a1f2504c487c1a10d7e10c2f
Greg Ogun <gregogun97@gmail.com>
2021-11-21 18:33:53 +0000

    chore: add utility style function

---
+++ styles/style.ts
@@ -0,0 +1,5 @@
+// this function is a utility that is to be used with any 3rd party/external components that require custom styling
+
+import { css } from 'stitches.config';
+
+export const style = css({});
